2b672dbf2155906cb10d6f0be75f4e802e4e22ea
jdmorales <you@example.com>
2015-10-31 05:52:59 -0500

    Cambiando configuración de base de datos

--- viao_projecy/settings.py
+++ viao_projecy/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'viao_projecy.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -97,8 +97,8 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -109,7 +109,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
